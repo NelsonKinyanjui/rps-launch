a7f7dd279ccd9595f1fefc36508dffc0c80593ca
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-19 01:27:36 +0300

    restructure javascript back to html

--- assets/launch.js
+++ assets/launch.js
@@ -1,31 +1,5 @@
- function rockMove(){
-    let randomNumber = Math.random();
-
-    let computerMove = '';
-
-    if(randomNumber > 0 && randomNumber < 1 / 3){
-        computerMove = 'rock';
-    }
-    else if(randomNumber >= 1 / 3 && randomNumber < 2 / 3){
-        computerMove = 'paper';
-    }
-     else if(randomNumber >= 2 / 3 && randomNumber < 1 ){
-        computerMove = 'scissors';
-    }
-     
-    let result = '';
-    if (computerMove === 'rock'){
-        result = 'You tie.';
-    }
-    else if (computerMove === 'paper'){
-        result = 'You win. ';
-    }
-    else if (computerMove === 'scissors'){
-        result = 'You Lose.';
-    }
-    
-
-    alert(`You picked Rock , computer picked ${computerMove} ,${result}`);
+ /*function rockMove(){
+   
  }
 
  function paperMove(){
@@ -57,6 +31,7 @@
 
     alert(`You picked Paper , computer picked ${computerMove} ,${result}`);
  }
+ 
  function scissorsMove(){
     let randomNumber = Math.random();
 
@@ -85,4 +60,4 @@
     
 
     alert(`You picked Scissors , computer picked ${computerMove} ,${result}`);
- }
+ } */
